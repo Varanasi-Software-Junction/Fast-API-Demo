0cb122c2ba0d51b8c571be176b885ebe7635de66
Champak's World <champaksworld@gmail.com>
2022-10-01 16:24:06 +0530

    simple get post method

--- main.py
+++ main.py
@@ -1,60 +1,39 @@
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from .database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+@app.get("/")
+def index():
+    return "Index"
+
+@app.get("/home/")
+def home():
+    return "Home"
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+#add two number
+@app.get("/add/{a}/{b}")
+def add(a,b):
+    a=int(a)
+    b=int(b)
+    d={"a":a,"b":b,"result":a + b}
+    # return "add" + str(int(a)+int(b))
+    return d
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
 
+@app.get("/sub/")
+def sub(a: int, b:int):
+    d={'a':a , 'b':b ,"result":a-b}
+    return d
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+@app.post("/mul/")
+def mul(a:int, b:int):
+    d={'a':a , 'b':b ,"result":a*b}
+    return d 
 
+@app.get("/mul/")
+def mul(a:int, b:int):
+    d={'a':a , 'b':b ,"result":a*b}
+    return d 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/itemsn/", response_model=schemas.Item)
-def create_item_for_user(
-        user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.post("/comments/{user_id}/citems/", response_model=schemas.Item)
-def create_comments_for_user(
-        user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+    
